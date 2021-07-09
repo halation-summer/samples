970af53b6a7d9cda7a55dc3398aef7eca81fd6a3
hs <you@example.com>
2021-07-09 16:20:10 +0900

    firebase 接続情報を外部ファイル化

--- app01/lib/firestore.js
+++ app01/lib/firestore.js
@@ -1,14 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdIpvxKkmACERuh2_FdfI6zo7Ki3Dd_RQ",
-  authDomain: "sample01-5fad1.firebaseapp.com",
-  projectId: "sample01-5fad1",
-  storageBucket: "sample01-5fad1.appspot.com",
-  messagingSenderId: "710609583806",
-  appId: "1:710609583806:web:690c92dbfa98cf1ce868ef"
-};
+import firebaseConfig from "lib/firebase-config";
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
