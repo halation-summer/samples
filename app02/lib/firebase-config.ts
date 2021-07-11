d3555850dd1030f6b43c7de2eced3c739390d881
hs <you@example.com>
2021-07-11 21:43:42 +0900

    firebase 接続情報を外部ファイル化

---
+++ app02/lib/firebase-config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6R5qaA9-rsIVPDEJxKfxmNzXt5MSzuSY",
+  authDomain: "app01-53333.firebaseapp.com",
+  projectId: "app01-53333",
+  storageBucket: "app01-53333.appspot.com",
+  messagingSenderId: "274655581674",
+  appId: "1:274655581674:web:927706a9d39ef7783bce93",
+  measurementId: "G-W2BBKK6M6Q"
+};
+
+export default firebaseConfig
+
--- app02/lib/firestore.ts
+++ app02/lib/firestore.ts
@@ -1,15 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD6R5qaA9-rsIVPDEJxKfxmNzXt5MSzuSY",
-  authDomain: "app01-53333.firebaseapp.com",
-  projectId: "app01-53333",
-  storageBucket: "app01-53333.appspot.com",
-  messagingSenderId: "274655581674",
-  appId: "1:274655581674:web:927706a9d39ef7783bce93",
-  measurementId: "G-W2BBKK6M6Q"
-};
+import firebaseConfig from "lib/firebase-config";
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
